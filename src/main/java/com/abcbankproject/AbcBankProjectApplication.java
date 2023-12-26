f043e495dbeab89f3729349b994cd01e0d9d6ffb
3edgeishwarya <ishwarya.k@3edge.in>
2023-12-26 14:42:09 +0530

    First Commit.

---
+++ src/main/java/com/abcbankproject/AbcBankProjectApplication.java
@@ -0,0 +1,13 @@
+package com.abcbankproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcBankProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcBankProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abcbankproject/AbcBankProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcbankproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcBankProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
